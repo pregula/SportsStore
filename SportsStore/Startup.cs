5c85a9fe2417c12333b2bf139af7bae9a58bc910
Paweł Reguła <pawel.regula92@gmail.com>
2019-02-05 13:16:50 +0100

    Add pagination

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -30,6 +30,12 @@ namespace SportsStore
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "pagination",
+                    template: "Produkty/Strona{productPage}",
+                    defaults: new { Controller = "Product", action = "List"});
+
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Product}/{action=List}/{id?}");
             });
