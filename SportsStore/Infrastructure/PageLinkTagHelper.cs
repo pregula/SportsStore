6930bf18e0f531dd9fb5c75f8281bdb7d6ca840d
Paweł Reguła <pawel.regula92@gmail.com>
2019-02-13 12:05:34 +0100

    Repair PageLinkTagHelper

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -43,7 +43,6 @@ namespace SportsStore.Infrastructure
                 TagBuilder tag = new TagBuilder("a");
                 PageUrlValues["productPage"] = i;
                 tag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
-                tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i });
                 if (PageClassesEnabled)
                 {
                     tag.AddCssClass(PageClass);
